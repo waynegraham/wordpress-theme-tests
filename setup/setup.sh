a3447342cd52c0046b6167528c580928000eb92b
Wayne Graham <wayne.graham@virginia.edu>
2015-03-02 12:28:57 -0500

    Setup Script debugging
    echo lines for debugging in Travis

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,10 +6,26 @@
 
 export WP_CORE_DIR=/tmp/wordpress
 
+echo "WP_CORE_DIR: $WP_CORE_DIR"
+
 mysql -e 'CREATE DATABASE wordpress_test;' -uroot
 
 wget -nv -O /tmp/wordpress.tar.gz https://github.com/WordPress/WordPress/tarball/$WP_VERSION
 mkdir -p $WP_CORE_DIR
 tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
 
+wget -nv -O $WP_CORE_DIR/wp-config.php https://raw.githubusercontent.com/waynegraham/wordpress-theme-tests/master/setup/wp-tests-config.php
 
+# put components in their proper directories
+theme_slug=$(basename $(pwd))
+theme_dir=$WP_CORE_DIR/wp-content/themes/$theme_slug
+
+echo "Theme slug: $theme_slug"
+echo "Theme dir: $theme_dir"
+
+cd ../
+mv $theme_slug $theme_dir
+
+cd $theme_dir
+
+echo "theme dir at end: $theme_dir"
